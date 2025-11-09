7516ecca5fa8c3c04a7bce314fd8b7066fc978de
Вуф <ivangrigorcuk180@gmail.com>
2025-11-09 20:47:53 +0200

    Lab9

--- furry/app_blog/models.py
+++ furry/app_blog/models.py
@@ -4,9 +4,9 @@ from django.urls import reverse
 
 class Category(models.Model):
     category = models.CharField(u'Категорія',
-                                max_length=250, help_text=u'Максимум 250 символів')
+       max_length=250, help_text=u'Максимум 250 сим.')
     slug = models.SlugField(u'Слаг')
-    objects = models.Manager()
+    objects = models.Manager()   
 
     class Meta:
         verbose_name = u'Категорія для публікації'
@@ -14,6 +14,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+
+    def get_absolute_url(self):
+        try:
+            url = reverse('articles-category-list',
+                          kwargs={'slug': self.slug})
+        except:
+            url = "/"
+        return url
 
 class Article(models.Model):
     title = models.CharField(u'Заголовок', max_length=250,
