6590ca374c98c6f2f79138a14efbf6c9332b6d14
Вуф <ivangrigorcuk180@gmail.com>
2025-11-10 18:27:54 +0200

    lab10

--- furry/furry/settings.py
+++ furry/furry/settings.py
@@ -134,3 +134,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static', ]
+    
+STATIC_ROOT = BASE_DIR / 'staticfiles'
